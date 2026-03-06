a0ab7b9e40d3ee9cfbd90d8fae6569d2ce21fb60
akgul-adem <adem.n.akgul@gmail.com>
2026-03-06 16:25:15 -0600

    add soldier class

---
+++ soldier.py
@@ -0,0 +1,21 @@
+import pygame  # Game library
+
+pygame.init()  # Initialize all pygame modules
+
+class Soldier(pygame.sprite.Sprite):
+    def __init__(self,x_pos,y_pos,scale ):
+        super().__init__()
+
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.scale = scale
+
+
+
+
+
+
+        original_img = pygame.image.load("./assets/img/player/Idle/0.png").convert_alpha()
+        self.image = pygame.transform.scale(surface= original_img, size= (original_img.get_width()*self.scale, original_img.get_height()*self.scale))
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.x_pos, self.y_pos)
